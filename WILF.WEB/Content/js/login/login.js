6360d5f5ac290007ad1ae4b7312be1cff8a4c7bc
wilfredoutp <1330357@utp.edu.pe>
2019-04-11 09:07:35 -0500

    3

---
+++ WILF.WEB/Content/js/login/login.js
@@ -0,0 +1,24 @@
+﻿$(function () { });
+function Logeo() {
+    var s = '';
+    if ($("#txtuser").val() == "") s += "Debe de ingresar un usuario\n";
+    if ($("#txtpassword").val() == "") s += "Debe de ingresar un password\n";
+    if (s != "") { alert(s); }
+    else {
+        $.getJSON("/Home/ValidaUsuario", { user: $("#txtuser").val(), pass: $("#txtpassword").val()}
+            , function (data) {
+                if (typeof(data) === 'object') {
+                    if (data.IdPerfil == 1) window.location.href = "/Cliente/index";
+                    else if (data.IdPerfil == 2) window.location.href = "/Cliente/Cliente";
+                }
+                else {
+                    alert(data);
+                }
+                data = undefined;
+            }).fail(function (jqxhr, textStatus, error) {
+                var err = textStatus + ", " + error;
+                alert(err);
+            });
+    }
+    return false;
+}
