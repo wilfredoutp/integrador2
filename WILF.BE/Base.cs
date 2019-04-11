807ce72be68b757709f3bd2fddec909338927ba4
wilfredoutp <1330357@utp.edu.pe>
2019-04-11 00:16:46 -0500

    entidades

---
+++ WILF.BE/Base.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WILF.BE
+{
+    public class Base
+    {
+        public int Estado { get; set; }
+        public DateTime Fecha { get; set; }
+        public Nullable<DateTime> FechaMod { get; set; }
+    }
+}
